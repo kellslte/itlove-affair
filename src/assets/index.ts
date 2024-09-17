467e958e8acedd29bb86ee079d2089cf3c075fb8
Edozie Onyeanusi <phynnedo@gmail.com>
2024-09-17 09:07:36 +0100

    feat: added a gallery section and wish section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,7 @@ import LeftBottom from "./left-bottom.png";
 import LeftTop from "./left-top.png";
 import RightBottom from "./right-bottom.png";
 import RightTop from "./right-top.png";
-import MainImage from "./main-image.png";
+import MainImage from "./main-image.webp";
 import PstIfeomaImg from "./pst_ifeoma.png";
 import PstTochiImg from "./pst_tochi.png";
 
