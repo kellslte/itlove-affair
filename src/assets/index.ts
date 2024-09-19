d7398a7e07c95bea10e42c35e3fe387c3cfdc144
Edozie Onyeanusi <phynnedo@gmail.com>
2024-09-19 16:32:50 +0100

    feat: finished with the home page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -8,6 +8,12 @@ import PstTochiImg from "./pst_tochi.png";
 import Gal1 from "./gal-1.webp";
 import Gal2 from "./gal-2.webp";
 import Gal3 from "./gal-3.webp";
+import Gal4 from "./gal-4.webp";
+import Gal5 from "./gal-5.webp";
+import Gal6 from "./gal-6.webp";
+import Gal7 from "./gal-7.webp";
+import Gal8 from "./gal-8.webp";
+import Gal9 from "./gal-9.webp";
 import NextIcon from "./next-icon.svg";
 import PreviousIcon from "./prev-icon.svg";
 import MblNextIcon from "./mbl-next-icon.svg";
@@ -24,6 +30,12 @@ export {
   Gal1,
   Gal2,
   Gal3,
+  Gal4,
+  Gal5,
+  Gal6,
+  Gal7,
+  Gal8,
+  Gal9,
   NextIcon,
   PreviousIcon,
   MblNextIcon,
