6e7c922d074f0aba9a63be8a692cae545d40d3a4
tochukwu670 <toochukwuali@gmail.com>
2024-10-21 04:10:57 +0100

    love story added and new images

--- src/pages/homePage/components/photoGallerySection.tsx
+++ src/pages/homePage/components/photoGallerySection.tsx
@@ -1,8 +1,15 @@
-import { Gal1, Gal5, Gal6, Gal7, Gal8, Gal9 } from "../../../assets";
 import Marquee from "react-fast-marquee";
 
 const PhotoGallerySection = () => {
-  const photos = [Gal9, Gal8, Gal7, Gal6, Gal5, Gal1];
+  const photos = [
+    "https://res.cloudinary.com/tochukwu/image/upload/v1729479575/wedding/court-1.webp",
+    "https://res.cloudinary.com/tochukwu/image/upload/v1727831558/wedding/vintage-goofy.webp",
+    "https://res.cloudinary.com/tochukwu/image/upload/v1729479575/wedding/court-2.webp",
+    "https://res.cloudinary.com/tochukwu/image/upload/v1727831185/wedding/jve5bas0nj7jvf5dnfcn.webp",
+    "https://res.cloudinary.com/tochukwu/image/upload/v1729479575/wedding/court-3.webp",
+    "https://res.cloudinary.com/tochukwu/image/upload/v1727831185/wedding/main-close.webp",
+    "https://res.cloudinary.com/tochukwu/image/upload/v1729479575/wedding/court-4.webp",
+  ];
 
   return (
     <section className="bg-white py-24">
@@ -11,11 +18,16 @@ const PhotoGallerySection = () => {
       </div>
       <div className="mt-16 flex flex-col gap-8">
         <div>
-        <Marquee pauseOnHover = {false} speed={20} direction="right">
-          {photos.map((photo, index) => (
-            <img src={photo} alt="Gallery" key={index} className="max-w-auto h-80 rounded-lg mr-8 object-cover mmd:max-w-48 mxs:h-64"/>
-          ))}
-        </Marquee>
+          <Marquee pauseOnHover={false} speed={20} direction="right">
+            {photos.map((photo, index) => (
+              <img
+                src={photo}
+                alt="Gallery"
+                key={index}
+                className="max-w-auto h-80 rounded-lg mr-8 object-cover mmd:max-w-48 mxs:h-64"
+              />
+            ))}
+          </Marquee>
         </div>
       </div>
     </section>
